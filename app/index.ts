d99709f3d42435dcffa2d60f68371afe57b29ee8
Hans <classsickpeople@gmail.com>
2023-06-02 23:50:17 +0800

    Initial commit

---
+++ app/index.ts
@@ -0,0 +1,16 @@
+import { Request, Response } from 'express'
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.use(express.json())
+
+app.listen(port, () => {
+  console.log(`\nApp Running on http://localhost:${port}`)
+  console.log('Press Ctrl-C to terminate\n')
+})
+
+app.get('/', async (req: Request, res: Response) => {
+  res.json({message: 'Welcome'})
+})
