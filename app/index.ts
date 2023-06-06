af8ed924c1d79c44f053d9b78f177e4af1490e6e
Hans <classsickpeople@gmail.com>
2023-06-06 22:02:05 +0800

    Rename file

--- app/index.ts
+++ app/index.ts
@@ -9,6 +9,6 @@ app.use(express.json())
 app.listen(port, () => {
   console.log(`\nApp Running on http://localhost:${port}`)
   console.log('Press Ctrl-C to terminate\n')
-});
+})
 
 app.use('/', indexRouter)
