e4da2c1eaaac74e59df65c731f797e2ff05879a0
Gilang <gilangmaulana541@gmail.com>
2023-06-07 22:42:46 +0700

    add login api

--- config/routes/index.ts
+++ config/routes/index.ts
@@ -2,6 +2,9 @@ const express = require('express')
 const { Users } = require('../../app/models/index')
 const { Request, Response, NextFunction } = require('express')
 
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+
 const router = express.Router()
 
 router.get('/', (req: typeof Request, res: typeof Response, next: typeof NextFunction) => {
@@ -20,6 +23,67 @@ router.get('/users', async (req: typeof Request, res: typeof Response, next: typ
     })
   } catch (err) {
     console.log(err)
+  }
+})
+
+router.post('/login', async (req: typeof Request, res: typeof Response, next: typeof NextFunction): Promise<any> => {
+  try {
+    const user = await Users.findOne({
+      where: {
+        username:req.body.username,
+        email: req.body.email,
+      }
+    })
+
+    console.log(user)
+
+    const match = await bcrypt.compare(req.body.password, user.password)
+    if (!match) return res.status(400).json({
+      message: 'password salah!'
+    })
+
+    const userId = user.id
+    const username = user.username
+    const email = user.email
+
+    const accessToken = jwt.sign({
+      userId,
+      email,
+      username
+    }, 'a3xyP2tSDk0zUxHviDxZ1NdCNPA0DOK1Z6qd6aG9EWl8vVLEnVWdEfJWpqHV5', {
+      expiresIn: '1d'
+    })
+
+    const refreshToken = jwt.sign({
+      userId,
+      email,
+      username
+    }, 'A6weD1Bef6pqF1yW4MZNplLpupLIWkc72Ij0VRXK5cxN1c25tilzGzap1OCT', {
+      expiresIn: '1d'
+    })
+
+    await Users.update({
+      refresh_token: refreshToken
+    }, {
+      where: {
+        id: userId
+      }
+    })
+
+    res.cookie('refreshToken', refreshToken, {
+      httpOnly: true,
+      maxAge: 24 * 60 * 60 * 1000
+    })
+
+    res.json({
+      accessToken
+    })
+
+  } catch (error) {
+    console.log(error)
+    res.status(404).json({
+      message: 'email tidak ditemukan!'
+    })
   }
 })
 
