1438491b7d2e5aea9df4ab31764cae51e037f16b
Gilang <gilangmaulana541@gmail.com>
2023-06-09 14:38:47 +0700

    edit flight n user list dengan konsep mvc

---
+++ app/controllers/userController.ts
@@ -0,0 +1,15 @@
+const { Users } = require('../models/index')
+
+module.exports = {
+     usersList: async (req: typeof Request, res: typeof Response, next: typeof NextFunction): Promise<any> => {
+        try {
+            const result = await Users.findAll()
+            return res.status(200).json({
+                status: 'SUCCESS',
+                users: result
+            })
+        } catch (err) {
+            console.log(err)
+        }
+    }
+}
--- config/routes/index.ts
+++ config/routes/index.ts
@@ -1,10 +1,9 @@
 const express = require('express')
-const { Users, Flights } = require('../../app/models/index')
-const { Request, Response, NextFunction } = require('express')
-const bcrypt = require('bcrypt')
-const jwt = require('jsonwebtoken')
-
 const router = express.Router()
+const { Request, Response, NextFunction } = require('express')
+
+const {flightList, flightDetail} = require('../../app/controllers/flightController')
+const {usersList} = require('../../app/controllers/userController')
 
 router.get('/', (req: typeof Request, res: typeof Response, next: typeof NextFunction) => {
   res.status(200).json({
@@ -13,41 +12,11 @@ router.get('/', (req: typeof Request, res: typeof Response, next: typeof NextFun
   })
 })
 
+router.get('/api/v1/users', usersList)
+
 // flights
-router.get('/api/v1/users', async (req: typeof Request, res: typeof Response, next: typeof NextFunction): Promise<any> => {
-  try {
-    const result = await Users.findAll()
-    return res.status(200).json({
-      status: 'SUCCESS',
-      users: result
-    })
-  } catch (err) {
-    console.log(err)
-  }
-})
-router.get('/flights', async (req: typeof Request, res: typeof Response, next: typeof NextFunction): Promise<any> => {
-  try {
-    const result = await Flights.findAll()
-    return res.status(200).json({
-      status: 'SUCCESS',
-      users: result
-    })
-  } catch (err) {
-    console.log(err)
-  }
-})
-router.get('/flights/:id', async (req: typeof Request, res: typeof Response, next: typeof NextFunction): Promise<any> => {
-
-  const result = await Flights.findOne({ where: { id: req.params.id } })
-  if (result === null) {
-    res.json({ status: 'ERROR', message: 'Data tidak ditemukan!' })
-  }
-  return res.status(200).json({
-    status: 'SUCCESS',
-    users: result
-  })
-
-})
+router.get('/flights', flightList)
+router.get('/flights/:id', flightDetail)
 
 
 export { }
