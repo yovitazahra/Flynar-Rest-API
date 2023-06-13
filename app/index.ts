1026a8db2e337d55b206a570117434821f89ecac
Hans <classsickpeople@gmail.com>
2023-06-13 12:23:18 +0800

    Setup cors and dotenv

--- app/index.ts
+++ app/index.ts
@@ -1,10 +1,14 @@
 const express = require('express')
+const cors = require('cors')
+const dotenv = require('dotenv')
 const indexRouter = require('../config/routes/index')
 
+dotenv.config()
 const app = express()
 const port = 3000
 
 app.use(express.json())
+app.use(cors({ origin: process.env.REMOTE_CLIENT_APP, credentials: true }))
 
 app.listen(port, () => {
   console.log(`\nApp Running on http://localhost:${port}/api/v1`)
