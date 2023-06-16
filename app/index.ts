a4a0680af444d484f1ac17b6d8db2dee4263a229
Hans <classsickpeople@gmail.com>
2023-06-16 10:37:26 +0800

    Create env PORT

--- app/index.ts
+++ app/index.ts
@@ -6,7 +6,7 @@ const indexRouter = require('../config/routes/index')
 
 dotenv.config()
 const app = express()
-const port = 8000
+const port = process.env.PORT || 8000
 
 app.use(express.json())
 app.use(cors({
